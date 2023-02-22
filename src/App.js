26e9735746c968a9e18f3459b2ec669e82e4cba2
Divami <mohan@divami.com>
2023-02-22 10:27:33 +0530

    worked on counter and parent to child communication

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+import Child from './Child'
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [inputData, setInputData] = useState("");
+  const handler = e => {
+    setInputData(e.target.value)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <h2> The count value is {count}</h2>
+      <div className='bts'>
+        <button onClick={() => setCount(count -1)}>Decrement</button>
+        <button onClick={() => setCount(count*0)}>Reset</button> 
+        <button onClick={() => setCount(count +1)}>Increment</button> 
+      </div>  
+    <br />
+    <input type="text" value={inputData} onChange={handler} placeholder="Give any input"/>
+    <br />
+    <div >
+    <Child className="child" countValue = {count} inputData = {inputData}/>
+    </div>
+    <br />
+    <div> The input given is : {inputData} </div>
+
     </div>
   );
 }
---
+++ src/Child.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+ function Child(props) {
+  return (
+    <div >
+      Count recieved to child {props.countValue}
+    </div>
+  )
+}
+
+export default Child;
